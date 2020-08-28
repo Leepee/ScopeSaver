044b5e2dab7bcd7e8facc0663171f50a67694145
Leeps <leepee2@yahoo.co.uk>
2020-08-28 23:48:57 +0100

    Setup now displays to the screen. T&H display good

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
 // - Adafruit Unified Sensor Lib: https://github.com/adafruit/Adafruit_Sensor
 
 #include "DHT.h"
-#include <ESP8266WiFi.h>        // Include the Wi-Fi library
+#include <ESP8266WiFi.h> // Include the Wi-Fi library
 #include <SPI.h>
 #include <Wire.h>
 #include <Adafruit_GFX.h>
@@ -16,33 +16,19 @@
 #define SCREEN_HEIGHT 64 // OLED display height, in pixels
 
 // Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
-#define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
+#define OLED_RESET -1 // Reset pin # (or -1 if sharing Arduino reset pin)
 
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
-#define NUMFLAKES     10 // Number of snowflakes in the animation example
+#define NUMFLAKES 10 // Number of snowflakes in the animation example
 
-#define LOGO_HEIGHT   16
-#define LOGO_WIDTH    16
+#define LOGO_HEIGHT 16
+#define LOGO_WIDTH 16
 
-const char* ssid     = "Bellapais";         // The SSID (name) of the Wi-Fi network you want to connect to
-const char* password = "KoubaCat37";     // The password of the Wi-Fi network
+const char *ssid = "Bellapais";      // The SSID (name) of the Wi-Fi network you want to connect to
+const char *password = "KoubaCat37"; // The password of the Wi-Fi network
 
-
-#define DHTPIN 0     // Digital pin connected to the DHT sensor
-// Feather HUZZAH ESP8266 note: use pins 3, 4, 5, 12, 13 or 14 --
-// Pin 15 can work but DHT must be disconnected during program upload.
-
-// Uncomment whatever type you're using!
-//#define DHTTYPE DHT11   // DHT 11
-#define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321
-//#define DHTTYPE DHT21   // DHT 21 (AM2301)
-
-// Connect pin 1 (on the left) of the sensor to +5V
-// NOTE: If using a board with 3.3V logic like an Arduino Due connect pin 1
-// to 3.3V instead of 5V!
-// Connect pin 2 of the sensor to whatever your DHTPIN is
-// Connect pin 4 (on the right) of the sensor to GROUND
-// Connect a 10K resistor from pin 2 (data) to pin 1 (power) of the sensor
+#define DHTPIN 0      // Digital pin connected to the DHT sensor
+#define DHTTYPE DHT22 // DHT 22  (AM2302), AM2321
 
 // Initialize DHT sensor.
 // Note that older versions of this library took an optional third parameter to
@@ -50,8 +36,8 @@ const char* password = "KoubaCat37";     // The password of the Wi-Fi network
 // as the current DHT reading algorithm adjusts itself to work on faster procs.
 DHT dht(DHTPIN, DHTTYPE);
 
-
-void testdrawchar(void) {
+void testdrawchar(void)
+{
   display.clearDisplay();
 
   display.setTextSize(1);      // Normal 1:1 pixel scale
@@ -61,102 +47,121 @@ void testdrawchar(void) {
 
   // Not all the characters will fit on the display. This is normal.
   // Library will draw what it can and the rest will be clipped.
-  for(int16_t i=0; i<256; i++) {
-    if(i == '\n') display.write(' ');
-    else          display.write(i);
+  for (int16_t i = 0; i < 256; i++)
+  {
+    if (i == '\n')
+      display.write(' ');
+    else
+      display.write(i);
   }
 
   display.display();
   delay(2000);
 }
 
-void testdrawstyles(void) {
+void testdrawstyles(void)
+{
   display.clearDisplay();
 
-  display.setTextSize(1);             // Normal 1:1 pixel scale
-  display.setTextColor(WHITE);        // Draw white text
-  display.setCursor(0,0);             // Start at top-left corner
+  display.setTextSize(1);      // Normal 1:1 pixel scale
+  display.setTextColor(WHITE); // Draw white text
+  display.setCursor(0, 0);     // Start at top-left corner
   display.println(F("Hello, world!"));
 
   display.setTextColor(BLACK, WHITE); // Draw 'inverse' text
   display.println(3.141592);
 
-  display.setTextSize(2);             // Draw 2X-scale text
+  display.setTextSize(2); // Draw 2X-scale text
   display.setTextColor(WHITE);
-  display.print(F("0x")); 
+  display.print(F("0x"));
   display.println(0xDEADBEEF, HEX);
 
   display.display();
   delay(2000);
 }
 
-void setup() {
+void setup()
+{
   Serial.begin(115200);
 
-
   Serial.println('\n');
-  
-  WiFi.begin(ssid, password);             // Connect to the network
-  Serial.print("Connecting to ");
-  Serial.print(ssid);
-  Serial.println(" ...");
 
-  int i = 0;
-  while (WiFi.status() != WL_CONNECTED) { // Wait for the Wi-Fi to connect
-    delay(1000);
-    Serial.print(++i); Serial.print(' ');
-  }
-
-  Serial.println('\n');
-  Serial.println("Connection established!");  
-  Serial.print("IP address:\t");
-  Serial.println(WiFi.localIP());         // Send the IP address of the ESP8266 to the computer
-
-  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
-  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { 
+    // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C))
+  {
     Serial.println(F("SSD1306 allocation failed"));
-    for(;;); // Don't proceed, loop forever
+    for (;;)
+      ; // Don't proceed, loop forever
   }
 
-    // Show initial display buffer contents on the screen --
-  // the library initializes this with an Adafruit splash screen.
-  // display.display();
-  delay(2000); // Pause for 2 seconds
 
   // Clear the buffer
   display.clearDisplay();
 
+  display.setTextSize(1);      // Normal 1:1 pixel scale
+  display.setTextColor(WHITE); // Draw white text
+  display.setCursor(0, 0);     // Start at top-left corner
 
-  // Show the display buffer on the screen. You MUST call display() after
-  // drawing commands to make them visible on screen!
+
+
+  WiFi.begin(ssid, password); // Connect to the network
+  Serial.print("Connecting to ");
+  Serial.print(ssid);
+  Serial.println(" ...");
+
+  display.println("Connecting to ");
+  display.print(ssid);
+  display.println(" ...");
   display.display();
-  delay(2000);
+
+  int i = 0;
+  while (WiFi.status() != WL_CONNECTED)
+  { // Wait for the Wi-Fi to connect
+    delay(1000);
+    Serial.print(++i);
+    Serial.print(' ');
+    display.print(++i);
+    display.print(' ');
+    display.display();
+  }
+
+  Serial.println('\n');
+  Serial.println("Connection established!");
+  Serial.print("IP address:\t");
+  Serial.println(WiFi.localIP()); // Send the IP address of the ESP8266 to the computer
+
+  display.clearDisplay();
+  display.setCursor(0, 0);     // Start at top-left corner
+
+  // display.println('\n');
+  display.println("Connection established!");
+  display.println("IP address:\t");
+  display.println(WiFi.localIP()); // Send the IP address of the ESP8266 to the computer
+  display.display();
+
+
   // display.display() is NOT necessary after every single drawing command,
   // unless that's what you want...rather, you can batch up a bunch of
   // drawing operations and then update the screen all at once by calling
   // display.display(). These examples demonstrate both approaches...
 
-
   // testdrawchar();      // Draw characters of the default font
 
   // testdrawstyles();    // Draw 'stylized' characters
 
-
-
   // Invert and restore display, pausing in-between
-  display.invertDisplay(true);
-  delay(1000);
-  display.invertDisplay(false);
-  delay(1000);
+  // display.invertDisplay(true);
+  // delay(1000);
+  // display.invertDisplay(false);
+  // delay(1000);
 
   // testanimate(logo_bmp, LOGO_WIDTH, LOGO_HEIGHT); // Animate bitmaps
-
-  Serial.println(F("DHTxx test!"));
 
   dht.begin();
 }
 
-void loop() {
+void loop()
+{
   // Wait a few seconds between measurements.
   delay(2000);
 
@@ -169,7 +174,8 @@ void loop() {
   float f = dht.readTemperature(true);
 
   // Check if any reads failed and exit early (to try again).
-  if (isnan(h) || isnan(t) || isnan(f)) {
+  if (isnan(h) || isnan(t) || isnan(f))
+  {
     Serial.println(F("Failed to read from DHT sensor!"));
     return;
   }
@@ -191,16 +197,16 @@ void loop() {
   Serial.print(hif);
   Serial.println(F("°F"));
 
-display.clearDisplay();
+  display.clearDisplay();
 
-display.setTextSize(1);      // Normal 1:1 pixel scale
+  display.setTextSize(1);      // Normal 1:1 pixel scale
   display.setTextColor(WHITE); // Draw white text
   display.setCursor(0, 0);     // Start at top-left corner
 
-display.print(F("Humidity: "));
-display.println(h);
-display.print(F("%  Temperature: "));
-display.print(t);
-display.display();
+  display.print(F("Humidity: "));
+  display.print(h);
+  display.println("%");
+  display.print(F("Temperature: "));
+  display.print(t);
+  display.display();
 }
-
